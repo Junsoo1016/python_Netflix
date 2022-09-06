6ee11627dc52a58ee9530f226672bdb7054a5e27
Jun <junsoopark92@gmail.com>
2022-09-06 10:56:30 -0400

    start project

--- django_netflix/urls.py
+++ django_netflix/urls.py
@@ -4,7 +4,6 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('core.urls',namespace='core')),
